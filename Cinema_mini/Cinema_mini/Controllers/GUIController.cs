0d627f990d12ece73196df5992ddcac37e8fa5d3
Minh Hieu <1454050037hieu@ou.edu.vn>
2018-02-06 23:19:56 +0700

    Up source va file can thiet

---
+++ Cinema_mini/Cinema_mini/Controllers/GUIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema_mini.Controllers
+{
+    public class GUIController : Controller
+    {
+        // GET: GUI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cinema_mini/Cinema_mini/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cinema_mini.Startup))]
+namespace Cinema_mini
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
